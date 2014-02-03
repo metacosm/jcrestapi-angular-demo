de139f640dee71deb7c4fd081c40d38185b0f7a1
Chris Laprun <metacosm@gmail.com>
2014-02-03 11:26:20 +0100

    Added display of potential error message.

--- app/js/services.js
+++ app/js/services.js
@@ -102,6 +102,8 @@ jcrServices.factory('DemoSession', function ($http) {
             });
 
             return sessions;
+        }, function(error) {
+            alert(error.data.message);
         });
     };
 
